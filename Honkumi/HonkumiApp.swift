de868048fbab19563721de6d289389894acbb3aa
s.orca.matsumoto <s.orca.matsumoto@gmail.com>
2026-05-26 12:41:17 +0900

    Initial Commit

---
+++ Honkumi/HonkumiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HonkumiApp.swift
+//  Honkumi
+//
+//  Created by 松本沙愛 on 2026/05/26.
+//
+
+import SwiftUI
+
+@main
+struct HonkumiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
